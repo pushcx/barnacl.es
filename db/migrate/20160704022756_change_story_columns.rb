cd218b57fdba90f81060cce9528cd33ebc3e329a
Peter Bhat Harkins <peter@push.cx>
2016-08-15 13:00:52 -0500

    postgres won't automatically cast integer to bool

--- db/migrate/20160704022756_change_story_columns.rb
+++ db/migrate/20160704022756_change_story_columns.rb
@@ -1,6 +1,13 @@
 class ChangeStoryColumns < ActiveRecord::Migration
   def change
-    change_column :stories, :is_moderated, :boolean, default: false, null: false
-    change_column :stories, :is_expired, :boolean, default: false, null: false
+    ActiveRecord::Base.connection.execute("ALTER TABLE stories ADD COLUMN is_moderated_bool BOOLEAN NOT NULL default false")
+    ActiveRecord::Base.connection.execute("UPDATE stories SET is_moderated_bool = true WHERE is_moderated = 1")
+    remove_column :stories, :is_moderated
+    rename_column :stories, :is_moderated_bool, :is_moderated
+
+    ActiveRecord::Base.connection.execute("ALTER TABLE stories ADD COLUMN is_expired_bool BOOLEAN NOT NULL DEFAULT false")
+    ActiveRecord::Base.connection.execute("UPDATE stories SET is_expired_bool = true WHERE is_expired = 1")
+    remove_column :stories, :is_expired
+    rename_column :stories, :is_expired_bool, :is_expired
   end
 end
